0fcd384c3d246a8772ab9e99fd660fdfe7825560
saulocapistrano <saulocapistrano@gmail.com>
2024-01-30 19:27:52 -0300

    client repository implements

--- src/main/java/ifinit/com/vendas/domain/repositories/ClientRepository.java
+++ src/main/java/ifinit/com/vendas/domain/repositories/ClientRepository.java
@@ -1,68 +1,25 @@
 package ifinit.com.vendas.domain.repositories;
 
 import ifinit.com.vendas.domain.entity.Client;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
-@Repository
-public class ClientRepository {
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 
+    @Query(value = "select * from Client c where c.name like '%:name%'", nativeQuery = true)
+    List<Client> findByNameLike(@Param("name") String name);
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    @Query("delete from Client c where c.name =:name")
+    @Modifying
+    void deleteClientsByName(String name);
+    boolean existsByName(String name);
 
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Client save(Client client){
-
-        entityManager.persist(client);
-        return client;
-    }
-
-
-    @Transactional
-    public Client update(Client client){
-    entityManager.merge(client);
-    return client;
-    }
-
-    @Transactional
-    public void delete(Client client){
-
-        if(!entityManager.contains(client)){
-            client = entityManager.merge(client);
-        }
-        entityManager.remove(client);
-
-    }
-
-    @Transactional
-    public void delete(Integer id){
-    Client client =  entityManager.find(Client.class, id);
-        delete(client);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Client> getByName(String name){
-    String jpql = "select c from Client c where c.name like :name";
-     TypedQuery<Client> query  = entityManager.createQuery(jpql, Client.class);
-     query.setParameter("name", "%"+name+"%");
-     return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Client> returnAll(){
-        return entityManager.createQuery("from Client", Client.class).getResultList();
-    }
-
-
+    @Query("select  c from Client c left join fetch c.ordereds where c.id =:id ")
+    ClientRepository findClientByFetchOrdereds(@Param( "id") Integer id);
 }
