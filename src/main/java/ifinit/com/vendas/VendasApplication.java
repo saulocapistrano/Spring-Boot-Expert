cfa9d90321e7777cfe3a7199a3d1e5118a2a85c0
saulocapistrano <saulocapistrano@gmail.com>
2024-01-30 19:29:51 -0300

    manipuling sales application with create objects by test

--- src/main/java/ifinit/com/vendas/VendasApplication.java
+++ src/main/java/ifinit/com/vendas/VendasApplication.java
@@ -1,70 +1,44 @@
 package ifinit.com.vendas;
 
 import ifinit.com.vendas.domain.entity.Client;
+import ifinit.com.vendas.domain.entity.Ordered;
 import ifinit.com.vendas.domain.repositories.ClientRepository;
+import ifinit.com.vendas.domain.repositories.OrderedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired ClientRepository clientRepository){
+    public CommandLineRunner init(@Autowired ClientRepository clientRepository,
+                                  @Autowired OrderedRepository orderedRepository)
+    {
       return args -> {
 
           System.out.println("Save client.");
-          Client c1 = new Client();
-          c1.setName("Saulo José");
-          clientRepository.save(c1);
+          Client  c1 = clientRepository.save(new Client("Saulo José"));
+          Client  c2 = clientRepository.save(new Client("Karla Santana"));
 
-          Client c2 = new Client();
-          c2.setName("Francisco José");
-          clientRepository.save(c2);
+          Ordered o1 = new Ordered();
+          o1.setClient(c1);
+          o1.setOrderDate(LocalDate.now());
+          o1.setTotalOrder(BigDecimal.valueOf(100));
 
-          Client c3 = new Client();
-          c3.setName("Karla Capistrano");
-          clientRepository.save(c3);
-
-          Client c4 = new Client();
-          c4.setName("Joaquin Capistrano");
-          clientRepository.save(c4);
-
-          System.out.println("List client.");
-          List<Client> allClients = clientRepository.returnAll();
-          allClients.forEach(System.out::println);
-
-          System.out.println("Updated client.");
-          allClients.forEach( c -> {c.setName(c.getName() + " refreshed");
-          clientRepository.update(c);
-
-           });
+          orderedRepository.save(o1);
 
 
-          allClients = clientRepository.returnAll();
-          allClients.forEach(System.out::println);
+          ClientRepository client = clientRepository.findClientByFetchOrdereds(c1.getId());
+          System.out.println(client);
+          System.out.println(clientRepository.getReferenceById(c1.getId()));
 
-          System.out.println("Get client by step character.");
-          clientRepository.getByName("Sau").forEach(System.out::println);
-          clientRepository.getByName("Capis").forEach(System.out::println);
-
-
-          System.out.println("Delete character.");
-          clientRepository.returnAll().forEach(c ->{
-              clientRepository.delete(c);
-          });
-
-          allClients = clientRepository.returnAll();
-
-          if (allClients.isEmpty()) {
-            System.out.println("No have client");
-          }else{
-              allClients.forEach(System.out::println);
-          }
+          orderedRepository.findByClient(c1).forEach(System.out::println);
 
       };
     }
