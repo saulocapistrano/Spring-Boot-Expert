7f32fda57a819261e62b0292baf9747944b407f2
saulocapistrano <saulocapistrano@gmail.com>
2024-01-30 19:28:46 -0300

    refactory class ordered

--- src/main/java/ifinit/com/vendas/domain/entity/Ordered.java
+++ src/main/java/ifinit/com/vendas/domain/entity/Ordered.java
@@ -1,14 +1,25 @@
 package ifinit.com.vendas.domain.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity
 public class Ordered {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
     private LocalDate orderDate;
+    @Column(name = "total_order", precision = 20, scale = 2)
     private BigDecimal totalOrder;
 
+    @OneToMany(mappedBy = "ordered")
+    private List<OrderedItem> orderedItems ;
     public Integer getId() {
         return id;
     }
@@ -39,5 +50,24 @@ public class Ordered {
 
     public void setTotalOrder(BigDecimal totalOrder) {
         this.totalOrder = totalOrder;
+    }
+
+    public List<OrderedItem> getOrderedItems() {
+        return orderedItems;
+    }
+
+    public void setOrderedItems(List<OrderedItem> orderedItems) {
+        this.orderedItems = orderedItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Ordered{" +
+                "id=" + id +
+                ", client=" + client +
+                ", orderDate=" + orderDate +
+                ", totalOrder=" + totalOrder +
+                ", orderedItems=" + orderedItems +
+                '}';
     }
 }
