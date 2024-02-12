f9ee1dc14f1811e6e4304bbe1720894c5d9f75bc
saulocapistrano <saulocapistrano@gmail.com>
2024-02-12 17:48:35 -0300

    refactore controller of product

--- src/main/java/ifinit/com/vendas/rest/controller/ProductController.java
+++ src/main/java/ifinit/com/vendas/rest/controller/ProductController.java
@@ -2,9 +2,13 @@ package ifinit.com.vendas.rest.controller;
 
 import ifinit.com.vendas.domain.entity.Product;
 import ifinit.com.vendas.domain.repositories.ProductRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,6 +32,55 @@ public class ProductController {
         return ResponseEntity.notFound().build();
     }
 
+
+    @PostMapping
+    public ResponseEntity<Product> save(@RequestBody Product newProduct){
+        Optional<Product> extistingProduct = productRepository.findByNameLike(newProduct.getName());
+        if (extistingProduct.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(extistingProduct.get());
+        } else{
+            Product saveProduct = productRepository.save(newProduct);
+            return ResponseEntity.status(HttpStatus.CREATED).body(saveProduct);
+        }
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Product> delete(@PathVariable Integer id){
+        Optional<Product> extistingProduct = productRepository.findById(id);
+        if (extistingProduct.isPresent()){
+            productRepository.delete(extistingProduct.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<Product> update(@PathVariable Integer id, @RequestBody Product upProduct ){
+        Optional<Product> existingProduct = productRepository.findById(id);
+        if(existingProduct.isPresent()){
+            Product productToUpdate = existingProduct.get();
+            productToUpdate.setName(upProduct.getName());
+            productRepository.save(productToUpdate);
+            return ResponseEntity.ok(productToUpdate);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity findList(Product filter){
+
+        Example example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        List<Product> newList = productRepository.findAll(example);
+        return ResponseEntity.ok(newList);
+
+    }
 
 
 }
