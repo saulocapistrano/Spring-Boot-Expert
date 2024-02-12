bb8c8159c21f6e2b47226a654adbf198e60b6252
saulocapistrano <saulocapistrano@gmail.com>
2024-02-12 15:13:04 -0300

    create query builder to findByName

--- src/main/java/ifinit/com/vendas/domain/repositories/ProductRepository.java
+++ src/main/java/ifinit/com/vendas/domain/repositories/ProductRepository.java
@@ -2,8 +2,13 @@ package ifinit.com.vendas.domain.repositories;
 
 import ifinit.com.vendas.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-
+    @Query(value = "select * from Product p where p.name like :name", nativeQuery = true)
+    Optional<Product> findByNameLike(@Param("name") String name);
 }
