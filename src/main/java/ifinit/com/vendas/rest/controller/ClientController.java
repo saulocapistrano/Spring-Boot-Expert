3ab094467d43c11542a5834c1db1fdfeafa64792
saulocapistrano <saulocapistrano@gmail.com>
2024-02-12 10:47:30 -0300

    refactory method @RequestMapping

--- src/main/java/ifinit/com/vendas/rest/controller/ClientController.java
+++ src/main/java/ifinit/com/vendas/rest/controller/ClientController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/api/client")
+@RequestMapping(value = "/api/clients")
 
 public class ClientController {
 
