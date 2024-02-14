2ef0a7f5432004ae7833022c2d3afa1808ae5b66
saulocapistrano <saulocapistrano@gmail.com>
2024-02-14 15:04:37 -0300

    define method save in this service

--- src/main/java/ifinit/com/vendas/service/OrderedService.java
+++ src/main/java/ifinit/com/vendas/service/OrderedService.java
@@ -1,4 +1,8 @@
 package ifinit.com.vendas.service;
 
+import ifinit.com.vendas.domain.entity.Ordered;
+import ifinit.com.vendas.rest.dto.OrderedDTO;
+
 public interface OrderedService {
+    Ordered save(OrderedDTO orderedDTO);
 }
