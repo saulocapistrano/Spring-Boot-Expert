afe4c5c56d3936af9fc51b73cce07890a975f91e
saulocapistrano <saulocapistrano@gmail.com>
2024-02-16 17:56:57 -0300

    create end points

--- src/main/java/ifinit/com/vendas/rest/controller/OrderedController.java
+++ src/main/java/ifinit/com/vendas/rest/controller/OrderedController.java
@@ -1,20 +1,25 @@
 package ifinit.com.vendas.rest.controller;
 
+
 import ifinit.com.vendas.domain.entity.Ordered;
 import ifinit.com.vendas.rest.dto.OrderedDTO;
 import ifinit.com.vendas.service.OrderedService;
 import org.springframework.web.bind.annotation.*;
 
+
 import static org.springframework.http.HttpStatus.CREATED;
+
 
 @RestController
 @RequestMapping("/api/ordereds")
 public class OrderedController {
     private OrderedService orderedService;
 
+
     public OrderedController(OrderedService orderedService) {
         this.orderedService = orderedService;
     }
+
 
     @PostMapping
     @ResponseStatus(CREATED)
