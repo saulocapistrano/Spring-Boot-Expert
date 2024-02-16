b6bf59edc27ddbf765e2da8041cec27c0d06cce0
saulocapistrano <saulocapistrano@gmail.com>
2024-02-16 17:54:24 -0300

    refactore ordered item dto

--- src/main/java/ifinit/com/vendas/rest/dto/OrderedItemDTO.java
+++ src/main/java/ifinit/com/vendas/rest/dto/OrderedItemDTO.java
@@ -1,13 +1,30 @@
 package ifinit.com.vendas.rest.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderedItemDTO {
     private Integer product;
     private Integer quantity;
+
+    public OrderedItemDTO() {
+
+    }
+    public OrderedItemDTO(Integer product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Integer getProduct() {
+        return product;
+    }
+
+    public void setProduct(Integer product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
