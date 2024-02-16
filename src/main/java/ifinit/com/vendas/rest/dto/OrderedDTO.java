38b7fdf901b868a2bb64ed67e8777109026bda1f
saulocapistrano <saulocapistrano@gmail.com>
2024-02-16 17:54:08 -0300

    refactore ordered dto

--- src/main/java/ifinit/com/vendas/rest/dto/OrderedDTO.java
+++ src/main/java/ifinit/com/vendas/rest/dto/OrderedDTO.java
@@ -1,17 +1,69 @@
 package ifinit.com.vendas.rest.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import ifinit.com.vendas.domain.entity.Client;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class OrderedDTO {
     private Integer client;
     private BigDecimal total;
-    private List<OrderedItemDTO> orderedItemDTOS;
+    private Client name;
+    private List<OrderedItemDTO> orderedItemList;
+
+
+    public OrderedDTO(){
+
+    }
+    public OrderedDTO(Integer client, BigDecimal total, List<OrderedItemDTO> orderedItemList, Client name) {
+        this.client = client;
+        this.name = name;
+        this.total = total;
+        this.orderedItemList = orderedItemList;
+    }
+
+    public Integer getClient() {
+        return client;
+    }
+
+    public void setClient(Integer client) {
+        this.client = client;
+    }
+
+    public Client getName() {
+        return name;
+    }
+
+    public void setName(Client name) {
+        this.name = name;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<OrderedItemDTO> getOrderedItemList() {
+        return orderedItemList;
+    }
+
+    public void setOrderedItemList(List<OrderedItemDTO> orderedItemList) {
+        this.orderedItemList = orderedItemList;
+    }
+
+//    public BigDecimal calculatorAmount(BigDecimal amount) {
+//
+//        if (orderedItemList == null|| orderedItemList.isEmpty()) {
+//            throw new RulerManagerException("No have items to provide calculo.");
+//        }
+//        BigDecimal calc = orderedItemList.stream()
+//                .map(OrderedItemDTO::getAmount)
+//                .reduce(BigDecimal.ZERO, BigDecimal::add);
+//        return calc;
+//    }
+
 }
