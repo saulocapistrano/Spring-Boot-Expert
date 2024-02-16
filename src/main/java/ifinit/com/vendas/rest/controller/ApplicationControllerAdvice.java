d99e08b81f0563214e27c02d9931828410811a58
saulocapistrano <saulocapistrano@gmail.com>
2024-02-16 17:58:06 -0300

    work with controller advice

---
+++ src/main/java/ifinit/com/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package ifinit.com.vendas.rest.controller;
+
+import ifinit.com.vendas.exception.RulerManagerException;
+import ifinit.com.vendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RulerManagerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRulerManagerException(RulerManagerException exception){
+        String messageError = exception.getMessage();
+        return new ApiErrors(messageError);
+    }
+}
