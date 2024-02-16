2b6645f19e81871524f4bbb03c59cbdfea2f095f
saulocapistrano <saulocapistrano@gmail.com>
2024-02-16 17:56:10 -0300

    create instance of product and client to promove tests in postman

--- src/main/java/ifinit/com/vendas/VendasApplication.java
+++ src/main/java/ifinit/com/vendas/VendasApplication.java
@@ -1,6 +1,8 @@
 package ifinit.com.vendas;
 
+import ifinit.com.vendas.domain.entity.Client;
 import ifinit.com.vendas.domain.entity.Product;
+import ifinit.com.vendas.domain.repositories.ClientRepository;
 import ifinit.com.vendas.domain.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,14 +16,21 @@ import java.math.BigDecimal;
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner commandLineRunner(@Autowired ProductRepository productRepository){
+    public CommandLineRunner commandLineRunner(@Autowired ProductRepository productRepository, @Autowired ClientRepository clientRepository){
         return  args ->{
-            Product p1 = new Product(null, "IPhone 15 pro max" , new BigDecimal(1000), " Smartphone");
+            Product p1 = new Product(null, "IPhone 15 pro max" , new BigDecimal(10), " Smartphone");
             Product p2 = new Product(null," Mac M2" , new BigDecimal(7000), " MacBook");
             Product p3 = new Product(null, "Dell Inspirion i9 " ,new BigDecimal(5000), " Dell Inspirio i9 ");
             productRepository.save(p1);
             productRepository.save(p2);
             productRepository.save(p3);
+
+
+            Client c1 = new Client("Saulo", null, "12345678901");
+            Client c2 = new Client("Karla", null, "12345678901");
+            clientRepository.save(c1);
+            clientRepository.save(c2);
+
         };
     }
 
