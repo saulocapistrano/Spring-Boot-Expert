97541421ebaee119f6e7513511084e96fc60d7d8
saulocapistrano <saulocapistrano@gmail.com>
2024-02-19 07:10:23 -0300

    refactore

--- src/main/java/ifinit/com/vendas/service/OrderedService.java
+++ src/main/java/ifinit/com/vendas/service/OrderedService.java
@@ -3,6 +3,12 @@ package ifinit.com.vendas.service;
 import ifinit.com.vendas.domain.entity.Ordered;
 import ifinit.com.vendas.rest.dto.OrderedDTO;
 
+import java.util.Optional;
+
 public interface OrderedService {
     Ordered save(OrderedDTO dto);
+
+    Optional<Ordered> receiveCompletOrdered(Integer id);
+
+
 }
