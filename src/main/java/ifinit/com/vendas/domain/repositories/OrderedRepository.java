af2d878ca491c9077f9c1a44ea5179058cbc5404
saulocapistrano <saulocapistrano@gmail.com>
2024-02-19 07:08:39 -0300

    refactore ordered repository

--- src/main/java/ifinit/com/vendas/domain/repositories/OrderedRepository.java
+++ src/main/java/ifinit/com/vendas/domain/repositories/OrderedRepository.java
@@ -2,11 +2,16 @@ package ifinit.com.vendas.domain.repositories;
 
 import ifinit.com.vendas.domain.entity.Client;
 import ifinit.com.vendas.domain.entity.Ordered;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderedRepository extends JpaRepository<Ordered, Integer> {
 
     List<Ordered> findByClient(Client client);
+
+    @EntityGraph(attributePaths = "orderedItems")
+    Optional<Ordered> findById(Integer id);
 }
