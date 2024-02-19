44456ce6c8af0cfae36767524664090c0fd42e15
saulocapistrano <saulocapistrano@gmail.com>
2024-02-19 08:24:09 -0300

    create anum status to ordered

---
+++ src/main/java/ifinit/com/vendas/domain/enums/StatusOredered.java
@@ -0,0 +1,10 @@
+package ifinit.com.vendas.domain.enums;
+
+public enum StatusOredered {
+
+    CREATED(0, "CREATE_ORDERED"), CANCELED(1, "CANCEL_ORDER");
+
+    StatusOredered(int i, String string) {
+
+    }
+}
