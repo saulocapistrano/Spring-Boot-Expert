7c6e0870382e9a8347902d17d5a0d664926f6fd7
saulocapistrano <saulocapistrano@gmail.com>
2024-02-19 16:24:46 -0300

    assing service with update status

--- src/main/java/ifinit/com/vendas/service/OrderedService.java
+++ src/main/java/ifinit/com/vendas/service/OrderedService.java
@@ -1,14 +1,14 @@
 package ifinit.com.vendas.service;
 
 import ifinit.com.vendas.domain.entity.Ordered;
+import ifinit.com.vendas.domain.enums.StatusOredered;
 import ifinit.com.vendas.rest.dto.OrderedDTO;
 
 import java.util.Optional;
 
 public interface OrderedService {
     Ordered save(OrderedDTO dto);
-
     Optional<Ordered> receiveCompletOrdered(Integer id);
-
+    void updateStatus(Integer id, StatusOredered statusOredered);
 
 }
