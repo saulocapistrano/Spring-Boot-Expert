004a91c757b427e46641b23321f080fc2e179a47
saulocapistrano <saulocapistrano@gmail.com>
2024-02-22 18:28:44 -0300

    customize anotation from empty list for product

---
+++ src/main/java/ifinit/com/vendas/validation/NotEmptyList.java
@@ -0,0 +1,20 @@
+package ifinit.com.vendas.validation;
+
+import ifinit.com.vendas.validation.constraintvalidation.NotEmptyListValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = NotEmptyListValidator.class)
+public @interface NotEmptyList {
+    String message () default "This list no has empty";
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
