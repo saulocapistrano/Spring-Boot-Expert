569cb3996a3b1d004958c63b1ba9ccbd8de02f0c
saulocapistrano <saulocapistrano@gmail.com>
2024-02-22 18:31:17 -0300

    implemtent custom empty list for empty list

---
+++ src/main/java/ifinit/com/vendas/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,19 @@
+package ifinit.com.vendas.validation.constraintvalidation;
+
+import ifinit.com.vendas.validation.NotEmptyList;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List>{
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext context) {
+        return list != null && !list.isEmpty();
+    }
+}
