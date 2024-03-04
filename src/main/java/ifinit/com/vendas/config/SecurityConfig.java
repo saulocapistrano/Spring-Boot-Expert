c71e3fe9e4b6e1c78b0aeae6730f91b0d7812632
saulocapistrano <saulocapistrano@gmail.com>
2024-03-04 08:22:19 -0300

    feat: implement encoder, authenticate and security

--- src/main/java/ifinit/com/vendas/config/SecurityConfig.java
+++ src/main/java/ifinit/com/vendas/config/SecurityConfig.java
@@ -1,11 +1,44 @@
 package ifinit.com.vendas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig  {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("fulano")
+                .password(passwordEncoder()
+                        .encode("123")).roles("USER", "ADMIN");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/clients/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/products/**")
+                .hasRole("ADMIN")
+                .antMatchers("/api/ordereds/**")
+                .hasAnyRole("USER", "ADMIN")
+                .and()
+                .httpBasic();
+
+    }
 }
