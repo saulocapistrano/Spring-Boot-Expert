d69f622827baa5bd1838f6aebf77a94f5f724030
saulocapistrano <saulocapistrano@gmail.com>
2024-04-01 09:14:46 -0300

    create dto to credential

---
+++ src/main/java/ifinit/com/vendas/rest/dto/CredentialsDTO.java
@@ -0,0 +1,13 @@
+package ifinit.com.vendas.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredentialsDTO {
+
+private String login;
+private String password;
+
+}
